b6cdb04a2ed5efcc654b9c0cd48fd92185f7aa4b
Tanya Gupta <gupta.tanya@gmail.com>
2023-03-30 09:53:13 -0400

    Add class example

--- sandbox.py
+++ sandbox.py
@@ -5,8 +5,34 @@ from data_structures.referential_array import ArrayR
 
 from data_structures.array_sorted_list import ArraySortedList
 
+
+
+class Test:
+    def __init__(self, cols, rows):
+        self.cols = cols
+        self.rows = rows
+        self.data = [['X'] * cols for row in range(rows)]
+
+    def __iter__(self):
+        yield from self.data
+
+    def __getitem__(self, row):
+        return self.data[row]
+        # what should this be?
+
+
 def main():
     """ Main function """
+
+    t=Test (2,3)
+
+    
+
+    for x in t:
+        print(x)
+
+
+
 
     mygrid = Grid ("some", 3, 3)
     # look at the following
